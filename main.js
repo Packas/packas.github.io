e711f59a992e8e118a4b3ac633e5865bff955e8c
Packas <maxthomp2007@gmail.com>
2021-06-01 15:56:47 +1000

    Basic Spinning Box

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 import './style.css'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.BoxGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({color: 0x0000FF, wireframe: true});
+const Box = new THREE.Mesh(geometry, material);
+
+scene.add(Box);
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  Box.rotation.x += 0.01;
+  Box.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
