6a7bccfc8c84e7aa9d83c72483b775f111c5f6c1
Packas <maxthomp2007@gmail.com>
2021-06-06 18:22:31 +1000

    Testing Phase Starting

---
+++ main.js
@@ -0,0 +1,17 @@
+var container, camera, renderer, scene, crate;
+
+function setup(){
+    container = document.querySelector("./scene");
+    scene = new THREE.Scene();
+    
+    const fov = 35;
+    const aspect = window.innerWidth/window.innerHeight;
+    const clipping = {
+        near: 0.01,
+        far: 500
+    };
+    console.log(fov, aspect, clipping.far, clipping.near);
+    
+
+
+};
